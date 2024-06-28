acab1adef47d2b6f8807554f7c7fd8583ed30d2f
ALEXANDER KLEIN URLUV <151784158+alexanderkieinurluv@users.noreply.github.com>
2024-06-29 03:43:40 +0800

    Add files via upload
    rm -rf DAFFY
    https://github.com/alexanderkieinurluv/DAFFY
    git pull
    cd DAFFY
    python DAFFY.py

---
+++ DAFFY.py
@@ -0,0 +1,19 @@
+#coding=utf-8
+import os, sys, platform
+try:
+    if sys.argv[1]=='update':
+        os.system('rm -rf lava.so')
+except:
+    pass
+bit = platform.architecture()[0]
+if bit == '64bit':
+    if not os.path.isfile('lava.so'):
+        os.system('curl -L https://github.com/ALEXANDERKIEINURLUV/lava/blob/main/lava.cpython-311.so?raw=true -o tyrontest.so')
+        from tyrontest import main_menu
+        main_menu()
+    else:
+        from tyrontest import main_menu
+        main_menu()
+else:
+    print ('Your device is not supported ')
+    exit()
